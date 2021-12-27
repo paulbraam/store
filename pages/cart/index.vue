0c7d54947f092fdab34a796ff860c73e10df4fb9
paulbraam <paulbraam7@gmail.com>
2021-12-28 01:00:41 +0300

    Cart: added final price

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -4,14 +4,13 @@
       <p v-if="!cartItems.length" class="cart__placeholder">Корзина пуста</p>
       <div class="cart__items-grid">
         <CartItemCard v-for="item in cartItems" :key="item.id" :item="item" />
+        <div v-if="cartItems.length" class="cart__order-wrapper">
+          <h2>${{ getFinalPrice() }}</h2>
+          <button class="cart__order-button button-info" @click="handleOrder">
+            Оформить заказ
+          </button>
+        </div>
       </div>
-      <button
-        v-if="cartItems.length"
-        class="cart__order-button button"
-        @click="handleOrder"
-      >
-        Оформить заказ
-      </button>
     </div>
     <Modal
       v-show="isModalVisible"
@@ -29,6 +28,7 @@ import Component from 'vue-class-component';
 import { mapState, mapActions } from 'vuex';
 import CartItemCard from '../../components/CartItemCard.vue';
 import Modal from '../../components/Modal.vue';
+import { IItem } from '../../store/types';
 
 @Component({
   components: {
@@ -44,7 +44,9 @@ import Modal from '../../components/Modal.vue';
 })
 export default class Cart extends Vue {
   clearCart!: () => void;
+  finalPrice: string = '0';
   isModalVisible: boolean = false;
+  cartItems!: IItem[];
 
   showModal() {
     this.isModalVisible = true;
@@ -57,6 +59,12 @@ export default class Cart extends Vue {
   handleOrder() {
     this.clearCart();
     this.showModal();
+  }
+
+  getFinalPrice() {
+    return this.cartItems
+      .reduce((acc: number, item: IItem) => acc + item.count * item.price, 0)
+      .toFixed(2);
   }
 }
 </script>
@@ -72,6 +80,21 @@ export default class Cart extends Vue {
     padding: 0.2rem;
   }
 }
+.cart__order-wrapper {
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 5px;
+
+  @media (max-width: 965px) {
+    padding: 1rem;
+  }
+  @media (max-width: 481px) {
+    flex-direction: column;
+    padding: 0.2rem;
+  }
+}
 .cart__wrapper {
   width: 100%;
   position: relative;
@@ -80,7 +103,7 @@ export default class Cart extends Vue {
   text-align: center;
 }
 .cart__order-button {
-  margin: auto;
+  height: 35px;
 }
 .cart__title {
   text-align: center;
